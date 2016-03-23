e729f23c42966e36fa5958d32aaeceaa6c1c427d
haixiuxiong5 <398599843@qq.com>
2016-03-23 12:01:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 //////////
 // 原生js //
 //////////
-// window.onload = function() {
-//     waterfall('main', 'box');
-//     var dataInt = { "data": [{ "src": "2 栀子花开.jpg" }, { "src": "canzhan.png" }, { "src": "ab.jpg" }, { "src": "3.jpg" }] };
-//     window.onscroll = function() {
-//         if (checkScrollSlide) {
-//         	var oParent=document.getElementById('main');
-//             // 将数据块渲染到当前页面的尾部
-//             for (var i = 0; i < dataInt.data.length; i++){
-//             	var oBox=document.createElement('div');
-//             	oBox.className='box';
-//             	oParent.appendChild(oBox);
-//             	var oPic=document.createElement('div');
-//             	oPic.className='pic';
-//             	oBox.appendChild(oPic);
-//             	var oImg=document.createElement('img');
-//             	oImg.src='images/'+dataInt.data[i].src;
-//             	oPic.appendChild(oImg);
-//             }
-//             waterfall('main', 'box');
-//         }
-//     }
-// }
+window.onload = function() {
+    waterfall('main', 'box');
+    var dataInt = { "data": [{ "src": "2 栀子花开.jpg" }, { "src": "canzhan.png" }, { "src": "ab.jpg" }, { "src": "3.jpg" }] };
+    window.onscroll = function() {
+        if (checkScrollSlide) {
+        	var oParent=document.getElementById('main');
+            // 将数据块渲染到当前页面的尾部
+            for (var i = 0; i < dataInt.data.length; i++){
+            	var oBox=document.createElement('div');
+            	oBox.className='box';
+            	oParent.appendChild(oBox);
+            	var oPic=document.createElement('div');
+            	oPic.className='pic';
+            	oBox.appendChild(oPic);
+            	var oImg=document.createElement('img');
+            	oImg.src='images/'+dataInt.data[i].src;
+            	oPic.appendChild(oImg);
+            }
+            waterfall('main', 'box');
+        }
+    }
+}
 
-// function waterfall(parent, box) {
-//     // 将main下的所有的class为box的元素取出来
-//     var oParent = document.getElementById(parent);
-//     var oBoxs = getByClass(oParent, box);
-//     // 计算整个页面显示的列数
-//     var oBoxW = oBoxs[0].offsetWidth;
-//     var clos = Math.floor(document.body.clientWidth / oBoxW);
-//     console.log(clos);
-//     // 设置main的宽度
-//     oParent.style.cssText = 'width:' + oBoxW * clos + 'px;margin:0 auto';
-//     var hArr = [];
-//     for (var i = 0; i < oBoxs.length; i++) {
-//         if (i < clos) {
-//             hArr.push(oBoxs[i].offsetHeight);
-//         } else {
-//             var minH = Math.min.apply(null, hArr);
-//             // console.log(minH);
-//             var index = getMinHIndex(hArr, minH);
-//             oBoxs[i].style.position = 'absolute';
-//             oBoxs[i].style.top = minH + 'px';
-//             oBoxs[i].style.left = oBoxW * index + 'px';
-//             // oBoxs[i].style.left=oBoxs[index].offsetLeft+'px';
-//             hArr[index] += oBoxs[i].offsetHeight;
-//         }
-//     }
-// }
+function waterfall(parent, box) {
+    // 将main下的所有的class为box的元素取出来
+    var oParent = document.getElementById(parent);
+    var oBoxs = getByClass(oParent, box);
+    // 计算整个页面显示的列数
+    var oBoxW = oBoxs[0].offsetWidth;
+    var clos = Math.floor(document.body.clientWidth / oBoxW);
+    console.log(clos);
+    // 设置main的宽度
+    oParent.style.cssText = 'width:' + oBoxW * clos + 'px;margin:0 auto';
+    var hArr = [];
+    for (var i = 0; i < oBoxs.length; i++) {
+        if (i < clos) {
+            hArr.push(oBoxs[i].offsetHeight);
+        } else {
+            var minH = Math.min.apply(null, hArr);
+            // console.log(minH);
+            var index = getMinHIndex(hArr, minH);
+            oBoxs[i].style.position = 'absolute';
+            oBoxs[i].style.top = minH + 'px';
+            oBoxs[i].style.left = oBoxW * index + 'px';
+            // oBoxs[i].style.left=oBoxs[index].offsetLeft+'px';
+            hArr[index] += oBoxs[i].offsetHeight;
+        }
+    }
+}
 
-// // 根据class获取元素
-// function getByClass(parent, clsName) {
-//     var boxArr = [],
-//         /*用来存储获取到所有class为box的元素*/
-//         oElements = parent.getElementsByTagName('*');
-//     for (var i = 0; i < oElements.length; i++) {
-//         if (oElements[i].className == clsName) {
-//             boxArr.push(oElements[i]);
-//         }
-//     }
-//     return boxArr;
-// }
-
-
-// function getMinHIndex(arr, val) {
-//     for (var i in arr) {
-//         if (arr[i] == val) {
-//             return i;
-//         }
-//     }
-// }
+// 根据class获取元素
+function getByClass(parent, clsName) {
+    var boxArr = [],
+        /*用来存储获取到所有class为box的元素*/
+        oElements = parent.getElementsByTagName('*');
+    for (var i = 0; i < oElements.length; i++) {
+        if (oElements[i].className == clsName) {
+            boxArr.push(oElements[i]);
+        }
+    }
+    return boxArr;
+}
 
 
-// // 检测是否具备了滚动条加载数据库的条件
-// function checkScrollSlide() {
-//     var oParent = document.getElementById('main');
-//     var oBoxs = getByClass(oParent, 'box');
-//     var lastBoxH = oBoxs[oBoxs.length - 1].offsetTop + Math.floor(oBoxs[oBoxs.length - 1].offsetHeight / 2);
-//     var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-//     var height = document.body.clientHeight || document.documentElement.clientHeight;
-//     // console.log(height);
-//     return (lastBoxH < scrollTop + height) ? true : false;
+function getMinHIndex(arr, val) {
+    for (var i in arr) {
+        if (arr[i] == val) {
+            return i;
+        }
+    }
+}
 
-// }
+
+// 检测是否具备了滚动条加载数据库的条件
+function checkScrollSlide() {
+    var oParent = document.getElementById('main');
+    var oBoxs = getByClass(oParent, 'box');
+    var lastBoxH = oBoxs[oBoxs.length - 1].offsetTop + Math.floor(oBoxs[oBoxs.length - 1].offsetHeight / 2);
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.body.clientHeight || document.documentElement.clientHeight;
+    // console.log(height);
+    return (lastBoxH < scrollTop + height) ? true : false;
+
+}
 
 
 // jQuery
-$(window).on('load', function() {
+/*$(window).on('load', function() {
     waterfall();
     var dataInt = { "data": [{ "src": "2 栀子花开.jpg" }, { "src": "canzhan.png" }, { "src": "ab.jpg" }, { "src": "3.jpg" }] };
     $(window).on('scroll', function() {
@@ -128,4 +128,4 @@ function checkScrollSlide() {
     var scrollTop = $(window).scrollTop();
     var documentH = $(window).height();
     return (lastBoxDis < scrollTop + documentH) ? true : false;
-}
+}*/
